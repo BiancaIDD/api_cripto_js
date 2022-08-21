654ba253fcc783fc16f72075c9ae66911dd289a0
BiancaIDD <pastelitoesponjosoowo@gmail.com>
2022-08-20 23:42:21 -0500

    evento submit del formulario y función para conectar la api terminado

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,15 @@
+const cryptoForm = document.getElementById('crypto-form');
+
 async function fetchCryptoConversion(cryptoSymbol, currencySymbol) {
-  const apiKey = 'eb4efb03395c2b9ddf754369c88444bac8d34046d33829980b2ea90bde33b3c1';
-  const apiUrl = `https://min-api.cryptocompare.com/data/price?fsym=${cryptoSymbol}&tsyms=${currencySymbol}`;
-  
+  const apiKey =
+    'eb4efb03395c2b9ddf754369c88444bac8d34046d33829980b2ea90bde33b3c1';
+  let response = await fetch(
+    `https://min-api.cryptocompare.com/data/price?fsym=${cryptoSymbol}&tsyms=${currencySymbol}`
+  );
+  let conversion = await response.json();
+  return conversion;
 }
+
+cryptoForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+});
