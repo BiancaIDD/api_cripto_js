eeff261da10d4e230207b9e2edc87f65e14fd654
BiancaIDD <pastelitoesponjosoowo@gmail.com>
2022-08-20 22:25:38 -0500

    html inicial terminado

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,5 @@
-async function fetchCryptoConversion(cryptoSymbol, moneySymbol) {
+async function fetchCryptoConversion(cryptoSymbol, currencySymbol) {
   const apiKey = 'eb4efb03395c2b9ddf754369c88444bac8d34046d33829980b2ea90bde33b3c1';
+  const apiUrl = `https://min-api.cryptocompare.com/data/price?fsym=${cryptoSymbol}&tsyms=${currencySymbol}`;
+  
 }
